8782fd130a7eb16039b80b127108220f6ca5310b
Vitalii <svs_2008@ukr.net>
2023-04-22 18:33:47 +0300

    Setting up a connection to postgresql

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inital_db',
+        'USER': 'postgres',
+        'PASSWORD': '10150173',
+        'HOST': 'postgres-database-1.cyznohivoanl.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
